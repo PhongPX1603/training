513873674b17fc9a823dcfbb05c511a194a29cf7
phongpx.1603 <phongpx.1603@gmail.com>
2022-06-16 16:03:28 +0700

    update test

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import torch
 import argparse
 import utils
+import time
 
 from tqdm import tqdm
 
@@ -9,13 +10,20 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--config-path', type=str, default='config_yaml/config.yaml')
     parser.add_argument('--weight-path', type=str)
+    parser.add_argument('--log-path', type=str)
     args = parser.parse_args()
     
     config = utils.load_yaml(args.config_path)
-    stage = utils.eval_config(config=config)
+    stages = utils.eval_config(config=config)
     
-    valid_loader = stage['data']['valid_loader']
-    model = stage['model']
+    logger = stages['logger']
+    logger.log_dir = str(args.log_path)
+    logger = logger.get_logger('test')
+    
+    
+    
+    valid_loader = stages['data']['valid']
+    model = stages['model']
     
     checkpoint = torch.load(args.weight_path)
     state_dict = checkpoint['state_dict']
@@ -23,24 +31,27 @@ if __name__ == '__main__':
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model.load_state_dict(state_dict)
     model = model.to(device).eval()
-    metrics = stage['metrics']
+    metrics = stages['metrics']
     total_loss = 0.0
-    total_metrics = torch.zeros(len(metrics))
-    
-    criterion = stage['loss']
     
     config = utils.load_yaml(args.config_path)
-    stage = utils.eval_config(config=config)
+    stages = utils.eval_config(config=config)
     
-    accuracies, losses = [], []
+    metrics.reset
+    print(f'{time.asctime()} - START TESTING')
+    logger.info(f'{time.asctime()} - START TESTING')
     with torch.no_grad():
-        for i, (samples, targets) in enumerate(tqdm(valid_loader)):
-            samples, targets = samples.to(device), targets.to(device)
-            preds = model(samples)
-            correct = torch.sum(targets == preds.argmax(dim=1))
-            accuracy = correct.item() / targets.shape[0]
+        for params in tqdm(valid_loader):
+            params = [param.to(device) for param in params if torch.is_tensor(param)]
+            params[0] = model(params[0])
             
-            accuracies.append(accuracy)
-        
-    accuracy = sum(accuracies) / len(accuracies) if len(accuracies) else 0
-    print(f'accuracy: {accuracy * 100: .2f}%')
+            iter_metric = metrics.iteration_compute(
+                evaluator_name='valid',
+                output=(params)
+            )
+            metrics.update(iter_metric)
+            result = metrics.epoch_compute
+    print(f'{time.asctime()} - TESTING COMPLETE !!!')
+    logger.info(f"{time.asctime()} - VALID ACCURACY: {result['valid_accuracy']}")
+    print(f"{time.asctime()} - VALID ACCURACY: {result['valid_accuracy']}")
+    logger.info(f'{time.asctime()} - TESTING COMPLETE !!!')
